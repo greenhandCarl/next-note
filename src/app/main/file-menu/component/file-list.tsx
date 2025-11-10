38738126595cb1cfafd72c5e5bda6c3ca67197e9
Jin Zheng <jin.zheng.external@tech.zalando.hk>
2025-11-10 22:37:56 +0800

    add file list

--- src/app/main/file-menu/component/file-list.tsx
+++ src/app/main/file-menu/component/file-list.tsx
@@ -1,3 +1,34 @@
-const FileList = () => {}
+import style from './file-list.module.css'
+import { FileMarkdownFilled } from '@ant-design/icons';
+import classNames from 'classnames';
+
+const FileList= () => {
+
+  return <>
+    <ul className={style.list}>
+      {/* <li className={style.empty}>
+        <img className={style.emptyIcon} src={require('/static/menus/empty_note.png')} alt="empty" />
+        <div className={style.emptyText}>没有找到文件</div>
+        <div className={style.newBtn} onClick={onNewClick}>新建笔记</div>
+      </li> */}
+      <li className={classNames(style.item)}>
+        <div className={style.title}>
+            <FileMarkdownFilled className={style.fileIcon} />
+            <span className={style.titleText}>Three.js</span>
+        </div>
+        <div className={style.date}>2025.04.03</div>
+      </li>
+      <li className={classNames(style.item)}>
+        <div className={style.title}>
+            <FileMarkdownFilled className={style.fileIcon} />
+            <span className={style.titleText}>Three.js</span>
+        </div>
+        <div className={style.date}>2025.04.03</div>
+      </li>
+    </ul>
+  </>
+}
+
+
 
 export default FileList;
--- src/app/main/file-menu/index.tsx
+++ src/app/main/file-menu/index.tsx
@@ -1,12 +1,14 @@
 import style from './file.module.css'
 import SearchBar from './component/search-bar';
 import MenuTitle from './component/menu-title';
+import FileList from './component/file-list';
 
 const FileMenu = () => {
   return (
     <div className={style.menusContainer}>
       <SearchBar />
       <MenuTitle />
+      <FileList />
     </div>
   );
 };
